e316190a205297b25a73b8e974913d415aea3f49
yiyuegudu <ct1071317@163.com>
2021-02-23 22:15:09 +0800

    bdyz

--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
+import {
+  Button,
+  Layout,
+  Icon,
+  Drawer,
+  Radio,
+  Menu,
+  Form,
+  Input
+} from "ant-design-vue";
+import Authorized from "./components/Authorized";
+import Auth from "./directives/auth";
 
 Vue.config.productionTip = false;
 
@@ -12,6 +23,10 @@ Vue.use(Icon);
 Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
+Vue.use(Form);
+Vue.use(Input);
+Vue.component("Authorized", Authorized);
+Vue.use(Auth);
 
 new Vue({
   router,
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,8 +1,66 @@
 <template>
-  <div>分析页</div>
+  <div>
+    <Chart :option="chartOption" style="height:500px" />
+  </div>
 </template>
 
 <script>
-export default {};
+// import random from "lodash/random";
+// import axios from "axios";
+import request from "../../utils/request";
+import Chart from "../../components/Chart";
+export default {
+  data() {
+    return {
+      chartOption: {}
+    };
+  },
+  mounted() {
+    this.getChartData();
+    this.interval = setInterval(() => {
+      this.getChartData();
+      // this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+      //   () => random(100)
+      // );
+      // this.chartOption = { ...this.chartOption }; //非深度监听用，赋新值
+    }, 3000);
+  },
+  methods: {
+    getChartData() {
+      request({
+        url: "/api/dashboard/chart",
+        method: "get",
+        params: { ID: 12345 }
+      })
+        // axios
+        //   .get("/api/dashboard/chart", { params: { ID: 12345 } })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: "ECharts 入门示例"
+            },
+            tooltip: {},
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [
+              {
+                name: "销量",
+                type: "bar",
+                data: response.data
+              }
+            ]
+          };
+        });
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+  components: {
+    Chart
+  }
+};
 </script>
 <style lang="stylus"></style>
